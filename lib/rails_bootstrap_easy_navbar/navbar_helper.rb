5f1a1d34ae21fcd45c1f181208cb27defaa13923
psyqor <Mitchell.lane@live.com>
2013-02-11 13:43:29 +1300

    Update lib/rails_bootstrap_easy_navbar/navbar_helper.rb

--- lib/rails_bootstrap_easy_navbar/navbar_helper.rb
+++ lib/rails_bootstrap_easy_navbar/navbar_helper.rb
@@ -39,7 +39,7 @@ module RailsBootstrapEasyNavbar
 		    content_tag(:div, content_tag(:div, contents.join('').html_safe, class: 'container-fluid'), class: 'navbar-inner')
 		end
 
-		    # Creates HTML for a list of clickable tabs 
+		# Creates HTML for a list of clickable tabs 
 		def tabs(current_tab, nav_items)
         	nav_items.map do |tab_name, path|
 	            if path.is_a? Hash # A Hash here means the tab is made up of multiple links -> dropdown 
@@ -56,6 +56,7 @@ module RailsBootstrapEasyNavbar
 			content_tag(:li, nil, class: "divider-vertical")
 		end
 
+		# Creates HTML for a dropdown list of clickable items 
 		def drop_down_tab(title, items)
 			[content_tag(:a, dropdown_title(title), href: "#", class: "dropdown-toggle", 'data-toggle' => "dropdown"),
 				content_tag(:ul, drop_down_links(items), class: "dropdown-menu")
